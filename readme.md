26860564da8221a703653663b457b3147f147495
koto <koto@koto.reisen>
2019-06-17 17:58:57 -0400

    Added basic Hydrus API support for tagging images by hash

--- readme.md
+++ readme.md
@@ -4,17 +4,30 @@ scikit-image
 numpy  
 cntk-gpu  
 click  
-flask (for server)
+flask (for server)  
+[Hydrus API](https://gitlab.com/cryzed/hydrus-api)(for API intergration)
 
 # Usage
 Download model from https://koto.reisen/model.cntk and put in danbooru-resnet_custom_v2-p4/ or run get_model.sh for unix systems  
 ## For sidecar tagger
 > python tagger.py evaluate "danbooru-resnet_custom_v1-p4" "some image.jpg"  
+
 > python tagger.py evaluate-batch "danbooru-resnet_custom_v1-p4" "some folder"  
 
-or see --help option.
+or see `--help` option.
 ## For server
 > python server.py
+
+## For API intergration
+> python tagger evaluate-api "danbooru-resnet_custom_v1-p4" --hash 52f7ab1c5860ef3d9b71d0fc3e69676fb2c2da16deaa8cb474ef20043ef43f30 --api_key 466f2185417001876effabd9ab53f9447439958b0774bf50d262b109e598ee99  
+
+> python tagger evaluate-api "danbooru-resnet_custom_v1-p4" --input hashes.txt --api_key 466f2185417001876effabd9ab53f9447439958b0774bf50d262b109e598ee99  
+
+or see `python tagger evaluate-api --help`
+
+You can add a default `api_key` into `tagger.py` by adding it to the default variable.  
+```@click.option('--api_key', default="466f2185417001876effabd9ab53f9447439958b0774bf50d262b109e598ee99")```  
+like so.  
 
 ### Lookup script  
 ![filelookup](DeepDanbooru.png)
--- tagger.py
+++ tagger.py
@@ -5,6 +5,7 @@ import core
 import cntk
 import os
 import re
+from io import BytesIO
 
 # Uncomment if you want to use CPU forcely
 # cntk.try_set_default_device(cntk.device.cpu())
@@ -20,7 +21,7 @@ def main():
 @click.argument('project_path', type=click.Path(exists=True, resolve_path=True, file_okay=False, dir_okay=True))
 @click.argument('image_path', type=click.Path(exists=True, resolve_path=True, file_okay=True, dir_okay=False))
 @click.option('--threshold', default=0.5, help='Score threshold for result.')
-def evaluate_project(project_path, image_path, threshold):
+def evaluate_sidecar(project_path, image_path, threshold):
     model, tags = core.load_model_and_tags(project_path)
 
     image = core.load_image_as_hwc(
@@ -29,8 +30,7 @@ def evaluate_project(project_path, image_path, threshold):
         image, (2, 0, 1)), dtype=np.float32)  # transpose HWC to CHW (3x299x299)
 
     results = model.eval(image).reshape(tags.shape[0])  # array of tag score
-    filename = os.path.basename(image_path)
-    file = open(filename + ".txt", "a")
+    file = open(image_path + ".txt", "a")
     alltags = []
     regex = re.compile('score:\w+')
 
@@ -48,7 +48,7 @@ def evaluate_project(project_path, image_path, threshold):
 @click.argument('project_path', type=click.Path(exists=True, resolve_path=True, file_okay=False, dir_okay=True))
 @click.argument('folder_path', type=click.Path(exists=True, resolve_path=True, file_okay=False, dir_okay=True))
 @click.option('--threshold', default=0.5, help='Score threshold for result.')
-def evaluate_project_batch(project_path, folder_path, threshold):
+def evaluate_sidecar_batch(project_path, folder_path, threshold):
     model, tags = core.load_model_and_tags(project_path)
     image_path_list = core.get_files_recursively(folder_path)
 
@@ -76,6 +76,50 @@ def evaluate_project_batch(project_path, folder_path, threshold):
         for tag in filtered:
             file.write(tag + "\n")
     file.close()
+
+
+@main.command('evaluate-api')
+@click.argument('project_path', type=click.Path(exists=True, resolve_path=True, file_okay=False, dir_okay=True))
+@click.option('--hash', '-h', multiple=True)
+@click.option('--threshold', default=0.5, help='Score threshold for result.', show_default=True)
+@click.option('--api_key', default="")
+@click.option('--service', default="local tags", show_default=True)
+@click.option('--input', '-i', nargs=1, type=click.Path(exists=True, resolve_path=True, file_okay=True, dir_okay=False), help="Input file with hashes to lookup, 1 hash per line.")
+@click.option('--api_url', default="http://127.0.0.1:45869/", show_default=True)
+def evaluate_api(project_path, threshold, service, api_key, hash, api_url, input):
+    try:
+        import hydrus
+    except ImportError:
+        print("Hydrus API not found.\nPlease install Hydrus API Python module.\nhttps://gitlab.com/cryzed/hydrus-api")
+        exit()
+    REQUIRED_PERMISSIONS = [hydrus.Permission.AddTags]
+
+    model, tags = core.load_model_and_tags(project_path)
+    if input:
+        with open(input, 'r') as f:
+            hashes = [line.strip() for line in f]
+    for hash in hash:
+        cl = hydrus.Client(api_key)
+        print(f'tagging {hash}')
+        image_path = BytesIO(cl.get_file(hash_=hash))
+
+        image = core.load_image_as_hwc(
+            image_path, (299, 299))  # resize to 299x299x3
+        image = np.ascontiguousarray(np.transpose(
+            image, (2, 0, 1)), dtype=np.float32)  # transpose HWC to CHW (3x299x299)
+
+        results = model.eval(image).reshape(
+            tags.shape[0])  # array of tag score
+        alltags = []
+        regex = re.compile('score:\w+')
+
+        for i in range(len(tags)):
+            if results[i] > threshold:
+                alltags.append(tags[i])
+
+        filtered = [x for x in alltags if not regex.match(x)]
+        hash = [hash]
+        cl.add_tags(hash, {service: filtered})
 
 
 def evaluate_post(image_path, threshold):
